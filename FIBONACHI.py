7a4c253f26e6d3def673b3f4bb0cda13a642ee51
Ilnaz-Yakhin <84871861+Ilnaz-Yakhin@users.noreply.github.com>
2022-05-24 07:39:12 +0300

    Число Фибоначи

---
+++ FIBONACHI.py
@@ -0,0 +1,27 @@
+def Fibonacci(lys, val):
+    fibM_minus_2 = 1
+    fibM_minus_1 = 2
+    fibM = fibM_minus_1 + fibM_minus_2
+    while (fibM < len(lys)):
+        fibM_minus_2 = fibM_minus_1
+        fibM_minus_1 = fibM
+        fibM = fibM_minus_1 + fibM_minus_2
+    index = -1;
+    while (fibM > 1):
+        i = min(index + fibM_minus_2, (len(lys)-1))
+        if (lys[i] < val):
+            fibM = fibM_minus_1
+            fibM_minus_1 = fibM_minus_2
+            fibM_minus_2 = fibM - fibM_minus_1
+            index = i
+        elif (lys[i] > val):
+            fibM = fibM_minus_2
+            fibM_minus_1 = fibM_minus_1 - fibM_minus_2
+            fibM_minus_2 = fibM - fibM_minus_1
+        else :
+            return i
+    if(fibM_minus_1 and index < (len(lys)-1) and lys[index+1] == val):
+        return index+1;
+    return -1
+number = int(input("Введите число"))
+print(Fibonacci([1,2,3,4,5,6,7,8,9,10,11], number))
